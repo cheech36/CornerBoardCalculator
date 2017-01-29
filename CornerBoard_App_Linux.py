c7000c872611922dac3118c28743a0ebb65f0e4f
Rich <chich36@gmail.com>
2017-01-29 18:35:05 -0500

    Change Layout Manager to Grid

--- CornerBoard_App_Linux.py
+++ CornerBoard_App_Linux.py
@@ -24,38 +24,29 @@ class Main_Window(Frame):
         self.style = Style()
         self.style.theme_use("default")
         
-        frame_1 = Frame(self, relief=RAISED, borderwidth=1, height=1)
-        frame_1.pack(side=TOP, expand=True)
+        frame_1=self.parent
 
-        frame_2 = Frame(self, height=1)
-        frame_2.pack(expand=FALSE)
-        self.pack(fill=BOTH, expand=TRUE)
-
-        Item_txt = Text(frame_1, height=1, width = 20)
-        Item_txt.pack(side=TOP, padx=5, pady=5)
+        Item_ent = Entry(frame_1)
         Calc_btn = Button(frame_1, text="Calculate")
-        Calc_btn.pack(side=RIGHT, padx=5, pady=5)
+
+        Glue_lbl = Label(frame_1, text="Glue")
+        Paper_lbl = Label(frame_1, text="Paper")
+        Liner_lbl = Label(frame_1, text="Liner")
+
+        Glue_txt = Text(frame_1, height=1, width = 20)
+        Paper_txt = Text(frame_1, height=1, width = 20)
+        Liner_txt = Text(frame_1, height=1, width = 20)
+
+        Item_ent.grid(row = 0, rowspan=2)
+        Calc_btn.grid(row=0, column = 1, rowspan=2)
 
 
-        frame_3 = Frame(self, height=1)
-        frame_3.pack(expand=TRUE)
-        Glue_lbl = Label(frame_3, text="Glue")
-        Glue_lbl.pack(side=TOP)
-        Glue_txt = Text(frame_3, height=1, width = 20)
-        Glue_txt.pack(side=RIGHT)
-
-
-        frame_4 = Frame(self, height=1)
-        frame_4.pack(expand=FALSE)
-        Glue_lbl = Label(frame_4, text="Paper")
-        Glue_lbl.pack(side=TOP)
-
-        frame_5 = Frame(self, height=1)
-        frame_5.pack(expand=FALSE)
-        Glue_lbl = Label(frame_5, text="Liner")
-        Glue_lbl.pack(side=LEFT)
-
-
+        Glue_lbl.grid(row=2, column = 0, stick=W)
+        Glue_txt.grid(row=2, column = 1)
+        Paper_lbl.grid(row=3, column=0, stick=W)
+        Paper_txt.grid(row=3, column=1)
+        Liner_lbl.grid(row=4, column=0, stick=W)
+        Liner_txt.grid(row=4, column=1)
 
 def main():
     root = Tk()
