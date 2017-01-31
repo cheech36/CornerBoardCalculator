dd2c57c4427144c4119c4d5b61410ffbf5f1369b
Rich <chich36@gmail.com>
2017-01-30 21:24:08 -0500

    Move Paper Weight Calculations into Cornerboard Class

--- Parser.py
+++ Parser.py
@@ -10,30 +10,17 @@ class Backend_Manager():
     def __init__(self, file, path):
         self.data_path = os.path.join(path, file)
         data_path = os.path.join(path, file)
-        self.gauge_table = pd.read_csv(data_path)
+        self.lookup_table = pd.read_csv(data_path)
         self.Text_Fields = dict()
 
     def parse(self, item):
             print('Parsing Item {0}'.format(item))
             cb = CornerBoard_Item(item)
             description = cb.parse()
-
+            PAPER_WEIGHT = cb.get_paper_weight(self.lookup_table)
             self.update_txt('Desc', description)
+            self.update_txt('Paper', PAPER_WEIGHT)
             self.update_txt('Glue', 'Na')
-
-            legs, gauge, length = cb.get_measurements()
-            print(gauge)
-            gauge_range = self.lookup_neighbor_indicies(gauge)
-
-            if len(gauge_range) == 1:
-                pass
-                linear_density = float(self.gauge_table.ix[gauge_range][legs])
-
-                print('length: ', length)
-                PAPER_WEIGHT = str(linear_density * float(length) / 12)
-                self.update_txt('Paper',PAPER_WEIGHT)
-
-
 
     def update_txt(self, field, text):
         self.Text_Fields[field].delete('1.0',END)
@@ -43,30 +30,6 @@ class Backend_Manager():
     def set_text_dict(self, text_fields):
         self.Text_Fields = text_fields
 
-
-    def lookup_neighbor_indicies(self, gauge):
-        array = self.gauge_table['Thickness ']
-        value = gauge
-        idx = (np.abs(array-value)).argmin()
-        neighbor = array[idx]
-
-        if neighbor > value:
-            upper = idx
-            lower = idx + 1
-            return [upper, lower]
-
-        elif neighbor < value:
-            lower = idx
-            upper = idx - 1
-            return [upper, lower]
-
-        else:
-            return [idx]
-
-
-
-
-
 class CornerBoard_Item():
     def __init__(self, item_code):
         print('Creating CornerBoard Item')
@@ -74,7 +37,6 @@ class CornerBoard_Item():
         self.legs = []
         self.gauge = 0
         self.length = 0
-
         self.gauge_str = ''
         self.legs_str = ''
         self.full_description = ''
@@ -86,12 +48,11 @@ class CornerBoard_Item():
 
         if (measurements.find("DX") != -1):
             print ("DX Item")
-            legs_gauge, self.length = re.split("DX", measurements)
+            legs_gauge, self.length_str = re.split("DX", measurements)
             legs = legs_gauge[0:-3]
             _ , gauge_str = legs_gauge.split(legs)
-
             self.legs_str = str(legs)
-            self.length_str = str(self.length)
+            self.length = float(self.length_str)
 
             if (len(legs) == 2):
                 legsize = [int(legs[0]), int(legs[1])]
@@ -136,7 +97,6 @@ class CornerBoard_Item():
             elif(len(measurements) == 11):
                 pass
 
-
             else:
                 print('Special Item: Calculate by hand')
 
@@ -147,8 +107,6 @@ class CornerBoard_Item():
         else:
             y = 2
 
-
-
         self.leg1_str = self.legs_str[0:y]
         self.leg2_str = self.legs_str[y:]
 
@@ -157,10 +115,8 @@ class CornerBoard_Item():
         return self.full_description
 
 
-    def get_measurements(self):
-
+    def get_standard_legs_str(self):
         paper_size = sum(self.legs)
-
         if paper_size == 3:
             leg_dims  = '1.5x1.5'
         elif paper_size == 4:
@@ -173,12 +129,38 @@ class CornerBoard_Item():
             leg_dims = '4x4'
         else:
             print('Error: Invalid Legsize: ', paper_size, self.legs)
+            return 'Null'
+
+        return  leg_dims
+
+    def lookup_neighbor_indicies(self, table):
+        array = table['Thickness ']
+        value = self.gauge
+        idx = (np.abs(array-value)).argmin()
+        neighbor = array[idx]
+
+        if neighbor > value:
+            upper = idx
+            lower = idx + 1
+            return [upper, lower]
+        elif neighbor < value:
+            lower = idx
+            upper = idx - 1
+            return [upper, lower]
+        else:
+            return [idx]
+
+    def calc_linear_paper_density(self, table ):
+        self.standard_legs = self.get_standard_legs_str()
+        gauge_range = self.lookup_neighbor_indicies(table)
+        table_value = float(table.ix[gauge_range][self.standard_legs])
+        return table_value
+
+    def get_paper_weight(self,table):
+        linear_density = self.calc_linear_paper_density(table)
+        length_feet = self.length / 12
+        paper_weight   = linear_density * length_feet
+        return paper_weight
 
 
-        return  leg_dims, float(self.gauge), self.length
 
-
-
-def LookupTable():
-    def __init__(self):
-        pass
